3260d3237776d972bd4187accd1d9428c4f1b4be
helopia <75479527+helopia@users.noreply.github.com>
2023-05-11 11:59:38 +0200

    JSON integration

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Root from "./routes/root";
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
-
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+    },
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
 
